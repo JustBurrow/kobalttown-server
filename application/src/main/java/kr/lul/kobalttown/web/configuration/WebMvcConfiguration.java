091600b8b06ac8d96f39743f38f1e9969a750b48
Just Burrow <just.burrow@lul.kr>
2017-08-05 19:26:20 +0900

    (ADD) 외부 리소스에 액세스.
    소스, JAR 외부의 리소스(css, js)를 사용할 수 있도록 설정 추가.

---
+++ application/src/main/java/kr/lul/kobalttown/web/configuration/WebMvcConfiguration.java
@@ -0,0 +1,42 @@
+package kr.lul.kobalttown.web.configuration;
+
+import kr.lul.kobalttown.web.configuration.Constants.Properties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author justburrow
+ * @since 2017. 8. 5.
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+  @Value("${" + Properties.CSS_LOCATION + ":#{null}}")
+  private String cssLocation;
+  @Value("${" + Properties.JS_LOCATION + ":#{null}}")
+  private String jsLocation;
+
+  /**
+   * 프로젝트 로컬 레포지토리 혹은 JAR 파일 외부의 리소스를 사용하기 위해 핸들러를 추가한다.
+   * <p>
+   * 기본적으로 <code>CSS</code>, <code>JS</code>는 별도의 서버에서 처리하는 설계이지만,
+   * 해당 설정이 없는 개발 환경 등에서 사용하기 위한 기능이다.
+   *
+   * @param registry
+   */
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    // 외부 CSS 디렉토리가 설정되어 있을 때만 사용한다.
+    // TODO 설정 경로가 심볼릭 링크일 경우의 처리
+    if (null != this.cssLocation) {
+      registry.addResourceHandler("/css/**").addResourceLocations(this.cssLocation);
+    }
+
+    // 외부 JS 디렉토리가 설정되어 있을 때만 사용한다.
+    // TODO 설정 경로가 심볼릭 링크일 경우의 처리
+    if (null != this.jsLocation) {
+      registry.addResourceHandler("/js/**").addResourceLocations(this.jsLocation);
+    }
+  }
+}
