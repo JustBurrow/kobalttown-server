5be49b8b487857d1f915a17491fc6ef10f801e44
Just Burrow <just.burrow@lul.kr>
2017-08-05 21:56:28 +0900

    (ADD) DB, DB 연결 설정.

---
+++ application/src/main/java/kr/lul/kobalttown/web/configuration/JpaConfiguation.java
@@ -0,0 +1,52 @@
+package kr.lul.kobalttown.web.configuration;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.HibernateExceptionTranslator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @author justburrow
+ * @since 2017. 8. 5.
+ */
+@Configuration
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class JpaConfiguation {
+  @Bean
+  @ConfigurationProperties(prefix = "spring.datasource")
+  public DataSource dataSource() {
+    return DataSourceBuilder.create().build();
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+
+    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+    factory.setDataSource(dataSource());
+    factory.setPackagesToScan();
+    factory.setJpaVendorAdapter(adapter);
+
+    return factory;
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager() {
+    return new JpaTransactionManager(entityManagerFactory().getObject());
+  }
+
+  @Bean
+  public HibernateExceptionTranslator hibernateExceptionTranslator() {
+    return new HibernateExceptionTranslator();
+  }
+}
